962344eb7e9353689731a1edeb6c56b504b4f0ce
Rob Hoelz <rhoelz@inoc.com>
2011-11-22 13:28:58 -0600

    Don't complete scripts in my private files

--- rc.lua
+++ rc.lua
@@ -23,6 +23,26 @@ require 'mousebindings'
 require 'keybindings'
 require 'clientrules'
 
+-- don't consider scripts in my private
+-- files for tab completion
+local posix = require 'posix'
+local home  = os.getenv 'HOME'
+local path  = os.getenv 'PATH'
+local paths = {}
+
+local is_blacklisted = {
+  [ home .. '/bin' ]                 = true,
+  [ home .. '/useful-perl/scripts' ] = true,
+}
+
+for p in string.gmatch(path, '[^:]+') do
+  if not is_blacklisted[p] then
+    paths[#paths + 1] = p
+  end
+end
+
+posix.setenv('PATH', table.concat(paths, ':'))
+
 -- shortcut for naughty.notify
 function alert(msg)
   naughty.notify {

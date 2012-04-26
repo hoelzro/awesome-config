f38cc5805ff1b6ada94ac4f82cb053483c24dcd9
Rob Hoelz <rob@hoelz.ro>
2012-04-26 08:45:54 +0200

    Make our volume control more flexible

--- volume.lua
+++ volume.lua
@@ -1,7 +1,10 @@
 local rmatch  = require('rex_pcre').match
 local popen   = io.popen
 local execute = os.execute
+local format  = string.format
 local volume  = {}
+
+local sinkpattern   = 'Sink\\s*#(\\d+)'
 local mutepattern   = 'Mute:\\s*(yes|no)'
 local volumepattern = 'Volume:\\s*0:\\s*(\\d+)%'
 
@@ -17,46 +20,47 @@ end
 
 local function getstate()
   local sinks  = slurpcommand('pactl list sinks')
+  local sinkno = rmatch(sinks, sinkpattern)
   local state  = rmatch(sinks, mutepattern)
   local volume = rmatch(sinks, volumepattern)
 
-  return volume, state ~= 'no'
+  return sinkno, volume, state ~= 'no'
 end
 
 function volume.get()
-  local volume = getstate()
+  local _, volume = getstate()
   return volume
 end
 
 function volume.ismute()
-  local _, mute = getstate()
+  local _, _, mute = getstate()
   return mute
 end
 
 function volume.toggle()
-  local mute = volume.ismute()
+  local sinkno, _, mute = getstate()
 
-  execute('pactl set-sink-mute 0 ' .. (mute and '0' or '1'))
+  execute(format('pactl set-sink-mute %d %d', sinkno, (mute and 0 or 1)))
   return not mute
 end
 
 function volume.increment()
-  local volume = volume.get()
+  local sinkno, volume = getstate()
   volume = volume + 5
   if volume > 100 then
     volume = 100
   end
-  execute('pactl set-sink-volume 0 ' .. tostring(volume) .. '%')
+  execute(format('pactl set-sink-volume %d %d%%', sinkno, volume))
   return volume
 end
 
 function volume.decrement()
-  local volume = volume.get()
+  local sinkno, volume = getstate()
   volume = volume - 5
   if volume < 0 then
     volume = 0
   end
-  execute('pactl set-sink-volume 0 ' .. tostring(volume) .. '%')
+  execute(format('pactl set-sink-volume %d %d%%', sinkno, volume))
   return volume
 end
 

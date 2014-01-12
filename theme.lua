0084a7313f1980c5aaefcd226317e0f8cdd4bbb6
Rob Hoelz <rob@hoelz.ro>
2014-01-12 16:44:47 +0100

    Use default awesome theme
    I'm getting tired of all that blue

--- theme.lua
+++ theme.lua
@@ -1,8 +1,8 @@
 local beautiful = require 'beautiful'
 local gears     = require 'gears'
 local home      = os.getenv 'HOME'
-beautiful.init(home .. '/.config/awesome/themes/sky/theme.lua')
+beautiful.init '/usr/share/awesome/themes/default/theme.lua'
 
 for s = 1, screen.count() do
-  gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+  gears.wallpaper.maximized('/usr/share/archlinux/wallpaper/archlinux-simplyblack.png', s, true)
 end

658a60d354798555dd09a7324d05af749b26ba93
Rob Hoelz <rob@hoelz.ro>
2014-07-23 16:25:25 -0500

    Make sure to apply custom config to urgent notifications

--- naughty-screen.lua
+++ naughty-screen.lua
@@ -1,3 +1,4 @@
+local merge_tables = require 'awful.util'.table.join
 local naughty = require 'naughty'
 local naughty_defaults = {
   opacity         = 0.75,
@@ -5,13 +6,13 @@ local naughty_defaults = {
   screen          = preferred_screen,
 }
 
-naughty.config.default_preset  = naughty_defaults
-naughty.config.presets.normal  = naughty_defaults
-naughty.config.presets.low     = naughty_defaults
-naughty.config.opacity         = naughty_defaults.opacity
-naughty.config.rounded_corners = naughty_defaults.rounded_corners
-naughty.config.screen          = naughty_defaults.screen
-naughty.config.mapping         = {
+naughty.config.presets.normal   = merge_tables(naughty.config.presets.normal, naughty_defaults)
+naughty.config.presets.low      = merge_tables(naughty.config.presets.low, naughty_defaults)
+naughty.config.presets.critical = merge_tables(naughty.config.presets.critical, naughty_defaults)
+naughty.config.opacity          = naughty_defaults.opacity
+naughty.config.rounded_corners  = naughty_defaults.rounded_corners
+naughty.config.screen           = naughty_defaults.screen
+naughty.config.mapping          = {
     {{urgency = '\0'}, naughty.config.presets.low},
     {{urgency = '\1'}, naughty.config.presets.normal},
     {{urgency = '\2'}, naughty.config.presets.critical}

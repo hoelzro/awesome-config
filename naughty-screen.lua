1be4bde90c0518fb586d8eca2973c75c81f7e4b0
Rob Hoelz <rob@hoelz.ro>
2018-04-26 20:18:33 -0500

    Update naughty configuration
    For one, to handle a small change in naming that must have happened in a
    fairly recent Awesome release
    
    For two, to limit icon sizes to 64x64

--- naughty-screen.lua
+++ naughty-screen.lua
@@ -1,18 +1,21 @@
 local merge_tables = require 'awful.util'.table.join
 local naughty = require 'naughty'
+local naughty_dbus = require 'naughty.dbus'
 local naughty_defaults = {
   opacity         = 0.75,
   rounded_corners = true,
   screen          = preferred_screen,
+  icon_size       = 128,
 }
 
-naughty.config.presets.normal   = merge_tables(naughty.config.presets.normal, naughty_defaults)
-naughty.config.presets.low      = merge_tables(naughty.config.presets.low, naughty_defaults)
-naughty.config.presets.critical = merge_tables(naughty.config.presets.critical, naughty_defaults)
-naughty.config.opacity          = naughty_defaults.opacity
-naughty.config.rounded_corners  = naughty_defaults.rounded_corners
-naughty.config.screen           = naughty_defaults.screen
-naughty.config.mapping          = {
+naughty.config.presets.normal           = merge_tables(naughty.config.presets.normal, naughty_defaults)
+naughty.config.presets.low              = merge_tables(naughty.config.presets.low, naughty_defaults)
+naughty.config.presets.critical         = merge_tables(naughty.config.presets.critical, naughty_defaults)
+naughty.config.defaults.opacity         = naughty_defaults.opacity
+naughty.config.defaults.rounded_corners = naughty_defaults.rounded_corners
+naughty.config.defaults.screen          = naughty_defaults.screen
+naughty.config.defaults.icon_size       = naughty_defaults.icon_size
+naughty_dbus.config.mapping          = {
     {{urgency = '\0'}, naughty.config.presets.low},
     {{urgency = '\1'}, naughty.config.presets.normal},
     {{urgency = '\2'}, naughty.config.presets.critical}

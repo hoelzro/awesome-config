b314b2c8d77c44d08af0f55b0628e00b5802d85e
Rob Hoelz <rob@hoelz.ro>
2018-04-26 20:57:52 -0500

    Remove icon_size from naughty_config
    While it works great for downscaling large notification icons, it also
    upscales small ones, which doesn't look great

--- naughty-screen.lua
+++ naughty-screen.lua
@@ -5,7 +5,6 @@ local naughty_defaults = {
   opacity         = 0.75,
   rounded_corners = true,
   screen          = preferred_screen,
-  icon_size       = 128,
 }
 
 naughty.config.presets.normal           = merge_tables(naughty.config.presets.normal, naughty_defaults)
@@ -14,8 +13,8 @@ naughty.config.presets.critical         = merge_tables(naughty.config.presets.cr
 naughty.config.defaults.opacity         = naughty_defaults.opacity
 naughty.config.defaults.rounded_corners = naughty_defaults.rounded_corners
 naughty.config.defaults.screen          = naughty_defaults.screen
-naughty.config.defaults.icon_size       = naughty_defaults.icon_size
-naughty_dbus.config.mapping          = {
+
+naughty_dbus.config.mapping = {
     {{urgency = '\0'}, naughty.config.presets.low},
     {{urgency = '\1'}, naughty.config.presets.normal},
     {{urgency = '\2'}, naughty.config.presets.critical}

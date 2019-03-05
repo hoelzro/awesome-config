fd6a4424a170973a8fd553ca9cef41a4298139b3
Rob Hoelz <rob@hoelz.ro>
2019-03-04 22:07:30 -0600

    Update link to wallpaper
    For some reason archlinux-wallpaper 1.4-5 changed the path????

--- theme.lua
+++ theme.lua
@@ -4,5 +4,5 @@ local home      = os.getenv 'HOME'
 beautiful.init '/usr/share/awesome/themes/default/theme.lua'
 
 for s = 1, screen.count() do
-  pcall(gears.wallpaper.maximized, '/usr/share/archlinux/wallpaper/archlinux-simplyblack.png', s, true)
+  pcall(gears.wallpaper.maximized, '/usr/share/backgrounds/archlinux/archlinux-simplyblack.png', s, true)
 end

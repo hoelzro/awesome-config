94a7db48b5adb98d75a44687f55986d26a8d364a
Rob Hoelz <rob@hoelz.ro>
2021-09-15 16:37:35 -0500

    Convert audio.lua to use donut
    …rather than my dmux hack and shelling out to dbus-send

--- audio.lua
+++ audio.lua
@@ -1,24 +1,77 @@
 local awful = require 'awful'
+local donut = require 'donut'
+
+local MAX_VOLUME = 65536
 
 local audio = {}
 
-local latest_address
+local latest_proxy
 
-dbus.connect_signal('org.freedesktop.DBus.Properties', function(metadata, name)
-  if name ~= 'org.mpris.MediaPlayer2.Player' then
-    return
+local function on_volume_change(_, _, _, _, _, params)
+  local volume = params[1][1]
+  print(string.format('volume: %.0f%%', 100 * volume / MAX_VOLUME))
+end
+
+local function on_mute_change(_, _, _, _, _, params)
+  local is_muted = params[1]
+  print('muted?', is_muted)
+end
+
+donut.run(function()
+  local session_bus = assert(donut.get_session_bus())
+  local pulse_lookup = session_bus:proxy('org.PulseAudio1', '/org/pulseaudio/server_lookup1')
+  local pulse_bus_address = assert(pulse_lookup:Get('org.PulseAudio.ServerLookup1', 'Address'))
+  local pulse_bus = assert(donut.get_bus(pulse_bus_address))
+
+  local pulse_core = pulse_bus:proxy('org.PulseAudio.Core1', '/org/pulseaudio/core1')
+  assert(pulse_core:ListenForSignal('org.PulseAudio.Core1.Device.VolumeUpdated', {}))
+  assert(pulse_core:ListenForSignal('org.PulseAudio.Core1.Device.MuteUpdated', {}))
+
+  pulse_bus:subscribe {
+    interface = 'org.PulseAudio.Core1.Device',
+    member    = 'VolumeUpdated',
+    callback  = on_volume_change,
+  }
+
+  pulse_bus:subscribe {
+    interface = 'org.PulseAudio.Core1.Device',
+    member    = 'MuteUpdated',
+    callback  = on_mute_change,
+  }
+
+  session_bus:subscribe {
+    object_path = '/org/mpris/MediaPlayer2',
+    interface   = 'org.freedesktop.DBus.Properties',
+    member      = 'PropertiesChanged',
+
+    -- XXX these are always run on the main coroutine, it seems - should I make that not the case?
+    callback = function(_, sender)
+      donut.run(function()
+        -- XXX detecting the latest_proxy getting off the bus would be nice!
+        latest_proxy = session_bus:proxy(sender, '/org/mpris/MediaPlayer2')
+      end, function() end)
+    end,
+  }
+
+  -- XXX detect who, if anyone, is currently playing?
+end, function(ok, err)
+  if not ok then
+    print('failed to initialize audio stuff:', err)
   end
-
-  latest_address = metadata.sender
 end)
 
 local function run_on_latest(cmd)
-  if not latest_address then
+  if not latest_proxy then
     return
   end
 
-  awful.spawn(string.format('dbus-send --type=method_call --dest="%s" /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.%s',
-    latest_address, cmd))
+  donut.run(function()
+    assert(latest_proxy[cmd](latest_proxy))
+  end, function(ok, err)
+    if not ok then
+      print(string.format('failed to run MPRIS command %q: %s'), cmd, err)
+    end
+  end)
 end
 
 function audio.next()

9d2e70eeb6c083ad359a4163c8e4113d6052f81b
Rob Hoelz <rob@hoelz.ro>
2022-08-11 17:44:05 -0500

    Remove references to removed screens
    Just some spring cleaning, nothing big

--- widgets.lua
+++ widgets.lua
@@ -306,3 +306,8 @@ awful.screen.connect_for_each_screen(function(s)
 
     mywibar[s]:set_widget(top)
 end)
+
+screen.connect_signal('removed', function(s)
+  mywibar[s] = nil
+  mypromptbox[s] = nil
+end)

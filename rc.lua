47f350a2f4f9b1cfbf65df3dbd843712e683fd44
Rob Hoelz <rob@hoelz.ro>
2022-12-23 13:04:02 -0600

    Run GC upon screen removal
    I noticed that memory usage tends to accumulate when I switch my laptop
    from a docked/undocked mode, and conducting experiments it looks like
    just running a GC when a screen is removed seriously curtails that
    build-up.  I'd love to figure out exactly what kind of garbage is
    hanging around, but I can always restart Awesome if things get bloated
    enough after a while, and this change helps stave off that bloat

--- rc.lua
+++ rc.lua
@@ -105,3 +105,7 @@ local awful = require 'awful'
 awful.screen.connect_for_each_screen(function(s)
   screen_count = screen_count + 1
 end)
+
+screen.connect_signal('removed', function()
+  collectgarbage 'collect'
+end)
